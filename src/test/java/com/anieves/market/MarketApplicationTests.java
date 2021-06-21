17532fc6f55de85c889c69ddc60ae18e2a7bb8d9
Alejo Nieves <alejonieves2000@gmail.com>
2021-06-21 07:08:31 -0500

    primer commit subiendo proyecto main

---
+++ src/test/java/com/anieves/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anieves.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
